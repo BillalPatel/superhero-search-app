d2ea5418e89f348264266f9e32c7f74fcf5386c4
billalp <billal.patel@rightmove.co.uk>
2019-10-30 10:17:11 +0000

    Pulling in dummy data to cards

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const data = [
+   {
+      "title": "Nissan",
+      "content": "Altima"
+   },
+   {
+      "title": "Ford",
+      "content": "Taurus"
+   },
+   {
+      "title": "Toyota",
+      "content": "Celica"
+   },
+   {
+      "title": "Cadillac",
+      "content": "SRX"
+   },
+   {
+      "title": "Toyota",
+      "content": "FJ Cruiser"
+   },
+   {
+      "title": "Nissan",
+      "content": "Altima"
+   },
+   {
+      "title": "Mercedes-Benz",
+      "content": "CL-Class"
+   },
+   {
+      "title": "Ford",
+      "content": "Explorer"
+   },
+   {
+      "title": "Dodge",
+      "content": "Ram 1500"
+   },
+   {
+      "title": "Lexus",
+      "content": "LS"
+   },
+   {
+      "title": "Acura",
+      "content": "Integra"
+   },
+   {
+      "title": "Mitsubishi",
+      "content": "Lancer"
+   },
+   {
+      "title": "Chevrolet",
+      "content": "Blazer"
+   },
+   {
+      "title": "Acura",
+      "content": "SLX"
+   },
+   {
+      "title": "Mercedes-Benz",
+      "content": "C-Class"
+   }
+];
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'tachyons'
 import Card from './Card';
+import { data } from './data';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 	<div>
-		<Card /> 
-		<Card /> 
-		<Card /> 
-		<Card /> 
+		<Card title={data[0].title} content={data[0].content}/> 
+		<Card title={data[1].title} content={data[1].content}/> 
+		<Card title={data[2].title} content={data[2].content}/> 
+		<Card title={data[3].title} content={data[3].content}/> 
+		<Card title={data[4].title} content={data[4].content}/> 
+		<Card title={data[5].title} content={data[5].content}/> 
+		<Card title={data[6].title} content={data[6].content}/> 
+		<Card title={data[7].title} content={data[7].content}/> 
+		<Card title={data[8].title} content={data[8].content}/> 
+		<Card title={data[9].title} content={data[9].content}/> 
+		<Card title={data[10].title} content={data[10].content}/> 
 	</div>,
 	document.getElementById('root'));
 
