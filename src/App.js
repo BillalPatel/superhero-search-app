2e27e31ad2e430d084de8a09dab64d315c3ccd0c
billalp <billal.patel@rightmove.co.uk>
2019-10-30 17:24:13 +0000

    Added state functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 import Header from './Header';
 import SearchBox from './SearchBox'
 import CardList from './CardList';
-import { data } from './data';
+import {data} from './data';
 
-const App = () => {
-	return (
-		<Fragment>
-			<Header />
-			<SearchBox />
-			<CardList data= { data }/>
-		</Fragment>
-	);
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			data: data,
+			searchField: ''
+		};
+	}
+
+	render() {
+		return (
+			<Fragment>
+				<Header />
+				<SearchBox />
+				<CardList data={this.state.data}/>
+			</Fragment>
+		);
+	}
 }
 
 export default App;
