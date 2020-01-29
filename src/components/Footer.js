533147799edcb4721d72201115287384a6e02570
billalp <billal.patel@rightmove.co.uk>
2020-01-29 12:00:15 +0000

    Tidy up code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 const Footer = () => {
-	return (
-		<footer className="pv4 ph3 ph5-m ph6-l mid-gray">
-		  <p className="f6 db tc">Data from: 
-		  	<a href="https://superheroapi.com/index.html" target="_blank">Superhero API</a>
-		  </p>
-		</footer>
-	)
-}
+  return (
+	<footer className="pv4 ph3 ph5-m ph6-l mid-gray">
+	  <p className="f6 db tc">Data from: 
+	    <a href="https://superheroapi.com/index.html" target="_blank" rel="noopener noreferrer">Superhero API</a>
+	  </p>
+	</footer>
+  )
+};
 
 export default Footer;
-
