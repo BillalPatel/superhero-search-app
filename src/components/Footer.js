b6f3da08312d9b0d4c7d746f14b5342025af4777
billalp <billal.patel@rightmove.co.uk>
2020-02-14 14:17:30 +0000

    Add space to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
 	<footer className="pv4 ph3 ph5-m ph6-l mid-gray">
-	  <p className="f6 db tc">Data from: 
-	    <a href="https://superheroapi.com/index.html" target="_blank" rel="noopener noreferrer">Superhero API</a>
+	  <p className="f6 db tc">Data from: <a href="https://superheroapi.com/index.html" target="_blank" rel="noopener noreferrer">Superhero API</a>
 	  </p>
 	</footer>
   )
