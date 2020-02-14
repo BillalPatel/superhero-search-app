e8a09b83e7026883c55dc8e739a259b2932f1350
billalp <billal.patel@rightmove.co.uk>
2020-02-14 15:39:54 +0000

    Text css fixed

--- src/components/SuperheroSearch.js
+++ src/components/SuperheroSearch.js
@@ -36,27 +36,29 @@ const SuperheroSearch = () => {
   });
 
   return (
-  	<div className='bg-light-green mw7 center pa3 br2-ns ba b--black-10'>
-	  <div className='cf'>
-		<form onSubmit={handleSearch}>
-		  <input 
-		    type='search' 
-		    placeholder='e.g. Batman or Spider-Man' 
-			onChange={handleChange}
-		    className='f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns'
-		  />
-		  <button
-		    type="submit"
-		    className='f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns'>
-			  Search
-		  </button>
-		</form>
+	<>
+  	  <div className='bg-light-green mw7 center pa3 br2-ns ba b--black-10'>
+	    <div className='cf'>
+		  <form onSubmit={handleSearch}>
+		    <input 
+		      type='search' 
+		      placeholder='e.g. Batman or Spider-Man' 
+			  onChange={handleChange}
+		      className='f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns'
+		    />
+		    <button
+		      type="submit"
+		      className='f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns'>
+			    Search
+		    </button>
+		  </form>
+	    </div>
 	  </div>
-	  {!searchError ?
-	  	<CardList data={filteredResults}/>
-		: <ErrorMessage />
+	  { !searchError ?
+	    <CardList data={filteredResults}/>
+	    : <ErrorMessage />
 	  }
-	</div>
+	</>
   )
 };
 
